eddead604dd67dbb04da97e79f3612e13c151171
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-12-07 20:05:20 +0300

    Modified css in DeleteBtn

--- client/src/styles/DeleteBtn.js
+++ client/src/styles/DeleteBtn.js
@@ -8,7 +8,7 @@ const DeleteBtnStyle = styled.button`
   display: inline-block;
   padding: 0.2em 0.6em;
   margin: 0 0.1em 0.1em 0;
-  color: #2b3099;
+  color: #14163c;
   border: 0.2em solid rgba(255, 255, 255, 0);
   border-radius: 2em;
   font-weight: 300;
@@ -17,9 +17,9 @@ const DeleteBtnStyle = styled.button`
   font-family: 'Roboto',sans-serif;
   -webkit-transition: all 150ms linear;
   :hover {
-    background: #c5b6d9;
+    background: #14163c;
     box-shadow: 1px 1px 2px rgba(255, 255, 255, 0.2);
-    color: #2b3099;
+    color: #fff;
     text-decoration: none;
     -webkit-transition: all 250ms linear;
             transition: all 250ms linear;
